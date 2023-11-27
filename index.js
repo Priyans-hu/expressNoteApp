a16fe441c3b729f27341904acaa1950b2c6a978d
priyans-hu <gargpriyanshu2004@gmail.com>
2023-11-27 12:15:07 +0530

    generated api routes to handle read write

--- index.js
+++ index.js
@@ -1,14 +1,66 @@
 // Imports and declarations
+const { randomUUID } = require('crypto');
 const express = require('express');
 const fs = require('fs');
 const app = express();
 const port = 8080;
-const db = './data/db.json';
+const { v4: uuidv4 } = require('uuid');
+const db = require('./data/db.json');
+const dbPath = './data/db.json';
 
 app.use(express.json());
 
 // Handleing the API request
+// Fetch notes from database
+app.get('/api/notes/', (req, res) => {
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+        if (data) {
+            res.send(JSON.parse(data)).statusCode = 200;
+        } else {
+            console.log(err);
+            res.send(err).statusCode = 500;
+        }
+    })
+})
 
+// Add notes to database
+app.post('/api/notes/', (req, res) => {
+    // Getting the request body to set as the notes object
+    const newNote = req.body;
+
+    // setting an unique id to each note
+    newNote.id = uuidv4();
+
+    // Reading the current database and then adding new note
+    db.push(newNote);
+
+    // Updating/Writting into database
+    fs.writeFileSync(dbPath, JSON.stringify(db));
+
+    res.send("Added new note").statusCode = 200;
+})
+
+// Deleting notes from database
+app.delete('/api/notes/:id', (req, res) => {
+    const noteID = req.params.id;
+
+    // Find the index of the note with the given ID
+    const noteIndex = db.findIndex(note => note.id.toString() === noteID);
+
+    // Check if the note with the given ID exists
+    if (noteIndex === -1) {
+        return res.status(404).send('Note not found');
+    }
+
+    // Remove the note from the array
+    db.splice(noteIndex, 1);
+
+    // Save the updated db to the file
+    fs.writeFileSync(dbPath, JSON.stringify(db));
+
+    // Send a response indicating success
+    res.status(200).send('Note removed successfully');
+});
 
 // Handleling paging of html files
 app.get('/', (req, res) => {
