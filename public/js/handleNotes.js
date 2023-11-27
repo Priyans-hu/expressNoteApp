78779be5677dff63105e1e8faa30ffcff220d029
priyans-hu <gargpriyanshu2004@gmail.com>
2023-11-27 20:35:56 +0530

    minor updates and style changes

--- public/js/handleNotes.js
+++ public/js/handleNotes.js
@@ -3,12 +3,12 @@ const getNotes = () => {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json(); // This returns another Promise
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // This returns another Promise
+        });
 };
 
 const renderSavedNotes = () => {
@@ -17,16 +17,17 @@ const renderSavedNotes = () => {
         // Now 'notes' is an array of objects
         if (notes.length <= 0) {
             document.getElementById('noteList').innerHTML = `
-                <li type="none" class="rounded border border-1 p-2 my-1 d-flex justify-content-between align-items-center">
+                <li type="none" class="rounded border p-2 my-1 d-flex justify-content-between align-items-center">
                     <p class="p-0 m-2">You have no Notes</p>
+                    <a href="/addNote"><button class="btn btn-primary m-2">Start Writing</button></a>
                 </li>
-                <a href="/addNote"><button class="btn btn-primary my-2">Add note</button></a>
             `;
         } else {
             document.getElementById('noteList').innerHTML = '';
             notes.forEach(note => {
                 document.getElementById('noteList').innerHTML += `
-                    <li type='none' class='rounded border border-1 p-2 my-1 d-flex justify-content-between align-items-center'>
+                    <li type='none' class='rounded border border-1 p-2 my-1 d-flex justify-content-between align-items-center' 
+                        onClick="renderActiveNote(${JSON.stringify(note)})">
                         <p class="p-0 m-2">${note.title}</p>
                         <i class="fa-solid fa-trash" data-hash-key="${note.id}" onclick="deleteNote(event)"></i>
                     </li>
@@ -36,10 +37,21 @@ const renderSavedNotes = () => {
     });
 }
 
+const renderActiveNote = (noteString) => {
+    const note = JSON.parse(noteString);
+    console.log(note);
+    document.getElementById('activeNote').innerHTML = `
+    <div>
+        <h2>${note.title}</h2>
+        <p>${note.description}</p>
+    </div>
+    `;
+}
+
 const deleteNote = (event) => {
     const id = event.target.getAttribute('data-hash-key');
     console.log(`deleting note ${id}`);
-    fetch (`/api/notes/${id}`, {
+    fetch(`/api/notes/${id}`, {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json' },
     })
